e1a1c2406cf0e7b47a6c685e94ef690cf0ae1674
Jobi <115511537+uniub@users.noreply.github.com>
2024-04-22 14:33:43 -0400

    asd

--- aboutblank.js
+++ aboutblank.js
@@ -46,7 +46,7 @@ function pagesecurity(baseURL) {
     if (currentURL.startsWith(baseURL)) {
         return;
     } else {
-        document.body.innerHTML = '<p style="color: black; font-family: arial; font-weight: 400; font-size: 32px;">[WARNING] This page is not being hosted on its original location, please go to <a style="color: black; font-family: arial; font-weight: 400; font-size: 32px;" href="universalub.github.io" target="_blank">universalub</a> to visit the original site.</p>';
+        document.body.innerHTML = '<p style="color: black; font-family: arial; font-weight: 400; font-size: 32px;">[WARNING] This page is not being hosted on its original location, please go to <a style="color: black; font-family: arial; font-weight: 400; font-size: 32px;" href="http://universalub.github.io" target="_blank">universalub</a> to visit the original site.</p>';
     }
 }
 
